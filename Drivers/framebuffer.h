f5afcbced52fa104ad742ef005deeab3457876fc
gihanthakavishan <94125710+gihanthakavishan@users.noreply.github.com>
2022-11-02 23:57:36 +0530

    Add files via upload

---
+++ Drivers/framebuffer.h
@@ -0,0 +1,3 @@
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
+
+void fb_move_cursor(unsigned short pos);
--- kmain.c
+++ kmain.c
@@ -1,5 +1,15 @@
+
 /* The C function */
-    int kmain(int arg1, int arg2, int arg3)
+    #define FB_GREEN     2
+    #define FB_DARK_GREY 8
+    #include "Drivers/framebuffer.h"
+    
+    void kmain()
     {
-        return arg1 + arg2 + arg3;
+
+	fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
+	fb_move_cursor(1000);
+        
+        
     }
+    
